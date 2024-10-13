11062a9cc5f2e40efc30c720f68b200c087e9de3
Francesc Teruel Rodríguez <francesc.tr03@gmail.com>
2024-10-14 00:13:44 +0200

    Main Menu + Client and Server Scenes Management

---
+++ Lab2_Sockets_FrancescTeruelRodríguez/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            LoadMenuScene();
+
+        }
+    }
+
+    public static void LoadMenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public static void LoadServerScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public static void LoadClientScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // Function to be called when the button is pressed
+    public void ExitGame()
+    {
+        // Exit the application
+        Application.Quit();
+
+        // This line is helpful for testing in the Unity editor
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+}
