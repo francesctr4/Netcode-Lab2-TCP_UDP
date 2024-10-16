5e454582fef76b40e77531ad192c93d13337c009
Francesc Teruel Rodríguez <francesc.tr03@gmail.com>
2024-10-16 09:31:49 +0200

    Fixed Client Scene Consoles

--- Lab2_Sockets_FrancescTeruelRodríguez/Assets/Scripts/Player/PlayerControllerClient.cs
+++ Lab2_Sockets_FrancescTeruelRodríguez/Assets/Scripts/Player/PlayerControllerClient.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerControllerClient : MonoBehaviour
 {
@@ -13,6 +15,8 @@ public class PlayerControllerClient : MonoBehaviour
     private bool nearUDP = false;
     private bool nearTCP = false;
 
+    private bool insideUI = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,30 +26,38 @@ public class PlayerControllerClient : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Check if the user is focused on a UI input field
+        bool isTypingInInputField = EventSystem.current.currentSelectedGameObject != null &&
+                                    EventSystem.current.currentSelectedGameObject.GetComponent<TMP_InputField>() != null;
+
         Vector3 speed = new Vector3(0, 0, 0);
-        if (Input.GetKey("d"))
-        {
-            // speed += Vector3.Cross(new Vector3(1, 0, 0), this.transform.forward) * movSpeed;
-            speed += new Vector3(1, 0, 0) * movSpeed;
-        }
-        if (Input.GetKey("a"))
-        {
-            //speed += Vector3.Cross(new Vector3(-1, 0, 0), this.transform.forward) * movSpeed;
-            speed += new Vector3(-1, 0, 0) * movSpeed;
 
-        }
-        if (Input.GetKey("w"))
+        if (!insideUI)
         {
-           // speed += Vector3.Cross(new Vector3(0, 0, 1), this.transform.forward) * movSpeed;
-            speed += new Vector3(0, 0, 1) * movSpeed;
+            if (Input.GetKey("d"))
+            {
+                // speed += Vector3.Cross(new Vector3(1, 0, 0), this.transform.forward) * movSpeed;
+                speed += new Vector3(1, 0, 0) * movSpeed;
+            }
+            if (Input.GetKey("a"))
+            {
+                //speed += Vector3.Cross(new Vector3(-1, 0, 0), this.transform.forward) * movSpeed;
+                speed += new Vector3(-1, 0, 0) * movSpeed;
 
-        }
-        if (Input.GetKey("s"))
-        {
-            //speed += Vector3.Cross(new Vector3(0, 0, -1), this.transform.forward) * movSpeed;
-            speed += new Vector3(0, 0, -1) * movSpeed;
+            }
+            if (Input.GetKey("w"))
+            {
+                // speed += Vector3.Cross(new Vector3(0, 0, 1), this.transform.forward) * movSpeed;
+                speed += new Vector3(0, 0, 1) * movSpeed;
 
-        }
+            }
+            if (Input.GetKey("s"))
+            {
+                //speed += Vector3.Cross(new Vector3(0, 0, -1), this.transform.forward) * movSpeed;
+                speed += new Vector3(0, 0, -1) * movSpeed;
+
+            }
+        }  
 
         this.gameObject.GetComponent<Rigidbody>().velocity = speed;
 
@@ -56,15 +68,16 @@ public class PlayerControllerClient : MonoBehaviour
         }
         else { animator.SetBool("isWalking", false); }
 
-        if (nearUDP && Input.GetKeyDown("e"))
+        if (nearUDP && Input.GetKeyDown("e") && !isTypingInInputField)
         {
             consoleUDP.SetActive(!consoleUDP.activeSelf);
+            insideUI = !insideUI;
 
         }
-        if (nearTCP && Input.GetKeyDown("e"))
+        if (nearTCP && Input.GetKeyDown("e") && !isTypingInInputField)
         {
             consoleTCP.SetActive(!consoleTCP.activeSelf);
-
+            insideUI = !insideUI;
         }
     }
 
